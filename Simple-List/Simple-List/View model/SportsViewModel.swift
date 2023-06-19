87acd619d22cb12c16e09167f4e7d77aa73c6ef7
Corey Duncan <see.duncan@gmail.com>
2023-06-19 16:10:35 +0300

    Sports view model (empty implementation)

---
+++ Simple-List/Simple-List/View model/SportsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ViewModel.swift
+//  Simple-List
+//
+//  Created by Corey Duncan on 19/6/23.
+//
+
+import Foundation
+
+struct SportsViewModel {
+    private(set) var sports:[Feature.Domain.Sport] = []
+
+}
