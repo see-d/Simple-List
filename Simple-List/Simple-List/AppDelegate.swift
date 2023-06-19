1e0df89fb7424b45044354a6a4d397ebb2b723a9
Corey Duncan <see.duncan@gmail.com>
2023-06-19 16:16:13 +0300

    Remove Main storyboard - implement root window on AppDelegate

--- Simple-List/Simple-List/AppDelegate.swift
+++ Simple-List/Simple-List/AppDelegate.swift
@@ -12,7 +12,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window:UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let testSports = [Feature.Domain.Sport.tennisFixture,
+                          Feature.Domain.Sport.f1Fixture]
+
+        let viewmodel = SportsViewModel(sports: testSports)
+        let initialViewController = SportsViewController(with: viewmodel)
+
+        self.window?.rootViewController = initialViewController
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 }
