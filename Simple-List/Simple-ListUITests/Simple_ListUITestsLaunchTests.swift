cd34b0d5a01b9f39d02d9bda15bbfb6ef567b7b3
Corey Duncan <see.duncan@gmail.com>
2023-06-19 12:56:08 +0300

    Create Project

---
+++ Simple-List/Simple-ListUITests/Simple_ListUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Simple_ListUITestsLaunchTests.swift
+//  Simple-ListUITests
+//
+//  Created by Corey Duncan on 19/6/23.
+//
+
+import XCTest
+
+final class Simple_ListUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
